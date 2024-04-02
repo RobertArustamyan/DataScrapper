cdd9230783f1d58c7ca71284561ac9246c0c748b
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-04-02 20:23:55 +0400

    Modified converting_to_excel.py

--- Functions/converting_to_excel.py
+++ Functions/converting_to_excel.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv("../Data/result.csv", encoding='utf-8')
-df.to_excel('output.xlsx', index=False)
+def converter():
+    df = pd.read_csv("../Data/result.csv", encoding='utf-8')
+    df.to_excel('output.xlsx', index=False)
